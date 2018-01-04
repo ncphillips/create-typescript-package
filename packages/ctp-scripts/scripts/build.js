e31fb6c747745aa164b290892e1cbf85bb896be0
ncphillips <ncphillips.19@gmail.com>
2018-01-04 17:46:06 -0400

    created a build script that runs rollup

---
+++ packages/ctp-scripts/scripts/build.js
@@ -0,0 +1,30 @@
+const path = require("path")
+const rollupTypescript = require("rollup-plugin-typescript")
+const package = require("../package.json")
+const rollup = require("rollup")
+
+const inputOptions = {
+  input: "src/index.ts",
+  plugins: [rollupTypescript()],
+}
+
+const outputOptions = {
+  file: `build/${package.name}.js`,
+  format: "cjs",
+}
+
+async function build() {
+  // create a bundle
+  const bundle = await rollup.rollup(inputOptions)
+
+  const { code, map } = await bundle.generate(outputOptions)
+
+  // or write the bundle to disk
+  await bundle.write(outputOptions)
+}
+
+try {
+  build()
+} catch (e) {
+  console.error("Failed to build Typescript project", e)
+}
