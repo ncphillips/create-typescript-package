4d9ca1bd31fcd21e4f5593c830d3cc68b017f5e4
ncphillips <ncphillips.19@gmail.com>
2018-01-04 21:09:24 -0400

    create-typescript-package basic commandline check

---
+++ packages/create-typescript-package/index.js
@@ -0,0 +1,28 @@
+const chalk = require("chalk")
+const commander = require("commander")
+const path = require("path")
+const packageJson = require(path.resolve(__dirname, "package.json"))
+
+let projectName
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments("<project-directory>")
+  .action(name => {
+    projectName = name
+  })
+  .allowUnknownOption()
+  .parse(process.argv)
+
+if (typeof projectName === "undefined") {
+  console.error("Please specify the project directory:")
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("<project-directory>")}`
+  )
+  console.log()
+  console.log("For example:")
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("my-typescript-package")}`
+  )
+  process.exit(1)
+}
